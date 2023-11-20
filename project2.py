dbc937621e02a4a73a9338f78ba3644042ab314e
Misagh <149416988+misagh213@users.noreply.github.com>
2023-11-19 19:15:33 -0800

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,3 @@
+#print adad 1 ta 3000
+for i in range(1, 3001):
+     print(i)
---
+++ project4.py
@@ -0,0 +1,9 @@
+#baresi yeki bodan pass
+password1 = input("Please enter the first password:")
+
+password2 = input("Please enter the second password:")
+
+if password1 == password2:
+     print("The passwords are the same:")
+else:
+     print("incorrect password:")
